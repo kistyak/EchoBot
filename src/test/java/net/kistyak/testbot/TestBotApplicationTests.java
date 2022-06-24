bb1b907e69d8068cbd7a8600f6c6b5ac6dcde24a
kistyak <kostian38045@gmail.com>
2022-06-24 22:54:23 +0300

    Initial commit

---
+++ src/test/java/net/kistyak/testbot/TestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kistyak.testbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
